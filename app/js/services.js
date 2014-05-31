071d0a983e8a91e9e82d17b66612289c3e42c3b9
joshuahutt <joshua@huttj.com>
2014-05-31 15:50:18 -0700

    Added Firebase integration
    Still struggling with realtime effects. Some of the collection-based
    attributes do not update correctly...

--- app/js/services.js
+++ app/js/services.js
@@ -6,22 +6,36 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
     .value('version', '0.1')
-    .service('userSvc', function($http){
+    .service('dbSvc', function(){
+        this.d = new Firebase('https://blinding-fire-1366.firebaseio.com/');
+    })
+    .service('userSvc', function($http, $q, dbSvc){
 
         var _users = [];
+        var _loaded = $q.defer();
 
-        this.load = function() {
-            return $http.get('../test/data/users.json').success(function(data) {
-                _users = data;
-            });
-        };
+//        this.load = function() {
+//            return $http.get('../test/data/users.json').success(function(data) {
+//                _users = data;
+//            });
+//        };
+
+        dbSvc.d.on('value', function(snapshot) {
+            _users = snapshot.val().users;
+            _loaded.resolve();
+            console.log(_users);
+        });
 
         this.init = function() {
-            if (!_users.length) {
-                return this.load();
-            }
-            return true;
-        }
+            return _loaded;
+        };
+
+//        this.init = function() {
+//            if (!_users.length) {
+//                return this.load();
+//            }
+//            return true;
+//        }
 
         this.getUser = function(username){
             for (var i = 0; i < _users.length; i++) {
@@ -42,8 +56,10 @@ angular.module('myApp.services', [])
             var users = [];
             for (var i = 0; i<_users.length; i++) {
                 for (var j = 0; j < _users[i].interestedIn.length; j++ ) {
-                    if (_users[i].interestedIn[j] === shortname){
-                        users.push(_users[i].username);
+                    if (_users[i].interestedIn) {
+                        if (_users[i].interestedIn[j] === shortname) {
+                            users.push(_users[i].username);
+                        }
                     }
                 }
             }
@@ -52,9 +68,11 @@ angular.module('myApp.services', [])
         this.getProjectMembers = function(shortname) {
             var users = [];
             for (var i = 0; i<_users.length; i++) {
-                for (var j = 0; j < _users[i].memberOf.length; j++ ) {
-                    if (_users[i].memberOf[j] === shortname){
-                        users.push(_users[i].username);
+                if (_users[i].memberOf) {
+                    for (var j = 0; j < _users[i].memberOf.length; j++ ) {
+                        if (_users[i].memberOf[j] === shortname){
+                            users.push(_users[i].username);
+                        }
                     }
                 }
             }
@@ -68,22 +86,32 @@ angular.module('myApp.services', [])
             return list;
         }
     })
-    .service('projectSvc', function($http){
+    .service('projectSvc', function($http, $q, dbSvc){
 
         var _projects = [];
+        var _loaded = $q.defer();
 
-        this.load = function() {
-            return $http.get('../test/data/projects.json').success(function(data) {
-                _projects = data;
-            });
-        };
+        dbSvc.d.on('value', function(snapshot) {
+            _projects = snapshot.val().projects;
+            _loaded.resolve();
+        });
 
         this.init = function() {
-            if (!_projects.length) {
-                return this.load();
-            }
-            return true;
-        }
+            return _loaded;
+        };
+
+//        this.load = function() {
+//            return $http.get('../test/data/projects.json').success(function(data) {
+//                _projects = data;
+//            });
+//        };
+//
+//        this.init = function() {
+//            if (!_projects.length) {
+//                return this.load();
+//            }
+//            return true;
+//        }
 
         this.getProject = function(shortname){
             for (var i = 0; i < _projects.length; i++) {
